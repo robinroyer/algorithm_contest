4bd9c7fab54875fe146bec2d7cf3780a68680198
robinroyer <ktyroby@hotmail.fr>
2016-12-09 00:03:35 -0500

    [edit] add back error msg

--- src/solution.cpp
+++ src/solution.cpp
@@ -43,7 +43,7 @@ void Solution::initSol(std::vector<Company*> companies) {
 		while (!tables_[k].addCompany(companies[j])) {
 			k++;
 			if (k >= tables_.size()) {
-				// No solution allowed
+				std::cout << " ... => Pas de Solution possible pour cette configuration" << std::endl;;
 				break;
 			}
 		}
